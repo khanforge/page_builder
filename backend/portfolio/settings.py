b86e5715eaae984ac216df2bd6c224185b6747ad
Parvej Khan <1999monustp@gmail.com>
2026-01-25 10:56:19 +0530

    udpated media settings

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 from datetime import timedelta
@@ -168,3 +168,8 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
+
+STATIC_ROOT = BASE_DIR/"staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = MEDIA_URL/"media"
