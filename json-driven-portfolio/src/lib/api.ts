38cc0129449b69a63f762d0a9e9935b0d320a0fc
Parvej Khan <1999monustp@gmail.com>
2026-01-25 22:30:17 +0530

    updated env var file

--- json-driven-portfolio/src/lib/api.ts
+++ json-driven-portfolio/src/lib/api.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL!;
 
 export async function fetchPages() {
     const res = await fetch(`${API_BASE_URL}/api/public/page/admin`)
