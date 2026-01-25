7a80cf1bca8d76270d7c6626eda5f3036a73fa3d
Parvej Khan <1999monustp@gmail.com>
2026-01-25 23:04:39 +0530

    replaced next env var to vite

--- json-driven-portfolio/src/lib/api.ts
+++ json-driven-portfolio/src/lib/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL!;
+  import.meta.env.VITE_API_BASE_URL!;
 
 export async function fetchPages() {
     const res = await fetch(`${API_BASE_URL}/api/public/page/admin`)
