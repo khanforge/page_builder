5e4a7df51970033b55fee5a6cb7ff3811838137c
Parvej Khan <1999monustp@gmail.com>
2026-01-31 00:11:02 +0530

    added new award section

--- backend/page_builder/management/commands/migrate_content.py
+++ backend/page_builder/management/commands/migrate_content.py
@@ -10,38 +10,43 @@ class Command(BaseCommand):
     file_path = Path(settings.BASE_DIR) / "page_builder/management/commands/data.html"
     page_slug = "home"
     profile_slug = "anurag"
-    component_title = "Miscellaneous links"
     
     def handle(self, *args, **kwargs):
         with open(self.file_path, 'r') as file:
             data = file.read()
         
         soup = BeautifulSoup(data, 'html.parser')
-        li_tags = soup.find_all('li')
-        with transaction.atomic():
-            try:
-                page = Page.objects.get(slug=self.page_slug, profile__slug = self.profile_slug)
-            except Page.DoesNotExist:
-                page = Page.objects.create(slug=self.page_slug, profile = Profile.objects.get(slug=self.profile_slug))
+        div_tags = soup.find_all('div')
 
-            try:
-                Component.objects.get(page=page, title=self.component_title)
-                return # Component already exists, skip creation
-            except Component.DoesNotExist:
-                pass
-            component = Component.objects.create(
-                page = page,
-                title = self.component_title,
-                layout = "publications",
+        li_tags = []
+        for div in div_tags:
+            li_tags = div.find_all('li')
+            title = div.find('h2').text if div.find('h2') else None
 
-            )
-            sub_component = SubComponent.objects.create(
-                component = component,
-            )
-            list_data = [li.decode_contents().strip() for li in li_tags if li and li.decode_contents().strip()]
-            content_block = ContentBlock.objects.create(
-                subcomponent = sub_component,
-                content_type = "list",
-                data  = list_data
-            )
-            print(f"Content Block created with id: {content_block.id}")
+            with transaction.atomic():
+                try:
+                    page = Page.objects.get(slug=self.page_slug, profile__slug = self.profile_slug)
+                except Page.DoesNotExist:
+                    page = Page.objects.create(slug=self.page_slug, profile = Profile.objects.get(slug=self.profile_slug))
+
+                try:
+                    Component.objects.get(page=page, title=title)
+                    return # Component already exists, skip creation
+                except Component.DoesNotExist:
+                    pass
+                component = Component.objects.create(
+                    page = page,
+                    title = self.component_title,
+                    layout = "publications",
+
+                )
+                sub_component = SubComponent.objects.create(
+                    component = component,
+                )
+                list_data = [li.decode_contents().strip() for li in li_tags if li and li.decode_contents().strip()]
+                content_block = ContentBlock.objects.create(
+                    subcomponent = sub_component,
+                    content_type = "list",
+                    data  = list_data
+                )
+                print(f"Content Block created with id: {content_block.id}")
